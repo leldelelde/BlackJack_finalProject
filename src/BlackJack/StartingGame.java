c8d0167c8d8fc1379de3c3eb7270618fa6dd6101
Lelde Zemberga <zembergalelde@gmail.com>
2023-04-23 20:31:08 +0300

    Few changes in main class

--- src/BlackJack/StartingGame.java
+++ src/BlackJack/StartingGame.java
@@ -1,13 +1,22 @@
 package BlackJack;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class StartingGame {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+        //This does not work at the moment...maybe because there are no database created on mySQL??
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection("https://github.com/leldelelde/BlackJack_finalProject.git", "username", "password");
+        } catch (SQLException e) {
+            System.out.println("Failed to connect to the database!");
+            e.printStackTrace();
+        }
 
         System.out.println("Welcome to BlackJack!");
         Scanner scanner = new Scanner(System.in);
@@ -26,37 +35,39 @@ public class StartingGame {
                 System.out.println("Sorry, you are too young for this game!");
             } else {
                 System.out.println("Please enter your username");
+                scanner.nextLine();
                 String newUserName = scanner.nextLine();
 
                 System.out.println("Please enter your password (it should be at least 7 characters long and consist at least one capital letter and at least one number)");
                 String newPassword = scanner.nextLine();
-                //there might be validation part for password - checking the length and use of capital letters and numbers
 
-                    if (Pattern.matches("^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).{7,}$", newPassword)){
-                        System.out.println("Your password does not meet the requirements!");
-                    }
-                    // I suppose there should be an option to fill the password untill it meets the requirements
-                System.out.println("Please enter your fullName");
-                String newFullName = scanner.nextLine();
+                if (Pattern.matches("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{7,}$", newPassword)) {
 
-                System.out.println("Please enter your email");
-                String newEmail = scanner.nextLine();
-                //there should be validation part, if email consists @ and "."
+                    System.out.println("Please enter your fullName");
+                    String newFullName = scanner.nextLine();
 
-                addDataOfRegistrationToDB(conn, newUserName, newPassword, newFullName, ageOfPlayer, newEmail);
-                //at the moment not sure how to solve the error of "conn"
-                // I realise that it is not recognised, but not sure, where to resolve it
-                System.out.println(newUserName + ", let's start the game!");
+                    System.out.println("Please enter your email");
+                    String newEmail = scanner.nextLine();
+                    //there should be validation part, if email consists @ and "." Maybe if there will be enough time.
+                    //If any of you have ideas how to do that - please, feel free to add lines
+
+                    //I commented out the part with connecting to DB, because at the moment it does not work
+                   // addDataOfRegistrationToDB(conn, newUserName, newPassword, newFullName, ageOfPlayer, newEmail);
+
+                    System.out.println(newUserName + ", let's start the game!");
+                } else {
+                    System.out.println("Your password does not meet the requirements!");
+                }
             }
-        } else {
+        }else{
             System.out.println("Ok, maybe next time!");
         }
-
-        // Start of the game - probably should be as a separate method, to make main class shorter
-
     }
+    //There should be login part for registered users - have to find, how to do this
 
-    public static void addDataOfRegistrationToDB (Connection conn, String username, String password, String fullName, int age, String email) throws SQLException {
+    // Start of the game - probably should be as a separate method, to make main class shorter.
+    //This might be checked when Justes class will be added to github
+    public static void addDataOfRegistrationToDB(Connection conn, String username, String password, String fullName, int age, String email) throws SQLException {
         String sql = "INSERT INTO users (username, password, fullName, age, email) VALUES (?, ?, ?, ?, ?)";
 
         PreparedStatement preparedStatement = conn.prepareStatement(sql);
@@ -68,7 +79,7 @@ public class StartingGame {
 
         int rowInserted = preparedStatement.executeUpdate();
 
-        if (rowInserted > 0){
+        if (rowInserted > 0) {
             System.out.println("Registration completed successfully");
         } else {
             System.out.println("Something went wrong!");
