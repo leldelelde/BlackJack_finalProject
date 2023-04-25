118035186236a4ad9149442ee2ecdd908822d9bb
Juste Domeikiene <j.peckyte@gmail.com>
2023-04-25 22:14:20 +0300

    Main, addCard

--- src/BlackJack/Main.java
+++ src/BlackJack/Main.java
@@ -4,16 +4,4 @@ public class Main {
     //Make some cards and a deck
     Deck testDeck = new Deck();
 
-//Add the cards to the deck
-<<<<<<< HEAD
-//testDeck.addCard(aCard);
-
-=======
-testDeck.addCard(aCard);
-testDeck.addCard(bCard);
-//testDeck.addCard(cCard);
->>>>>>> 3c70b7b212ca676fefb1a4083676ebfe7e7f8d3f
-//Print out the deck
-
-
 }
